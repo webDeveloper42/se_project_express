36b7fff97582bea7cf936cd1fa1b4368806d3e10
webDeveloper42 <rfrias361@gmail.com>
2026-04-02 08:08:28 -0400

    Fix:review errors

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const router = require("./routes/index");
-const auth = require("./middleware/auth");
-const { login, createUser } = require("./controllers/users");
-const { getClothesItems } = require("./controllers/clothingItems");
 
 const app = express();
 const { port = 3001 } = process.env;
@@ -12,16 +9,7 @@ const { port = 3001 } = process.env;
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 app.use(express.json());
 
-app.use((req, res, next) => {
-  req.user = { _id: "5d8b8592978f8bd833ca8133" };
-  next();
-});
-
 app.use(cors());
-app.post("/signup", createUser);
-app.post("/signin", login);
-app.get("/items", getClothesItems);
-app.use(auth);
 app.use(router);
 
 if (require.main === module) {
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const router = require("express").Router();
 const { NOT_FOUND_ERROR } = require("../utils/errors");
+const auth = require("../middleware/auth");
+const { createUser, login } = require("../controllers/users");
 const userRouter = require("./users");
 const clothingItemRouter = require("./clothingItems");
 
-router.use("/users", userRouter);
-router.use("/items", clothingItemRouter);
+router.post("/signup", createUser);
+router.post("/signin", login);
+
+router.use("/users", auth, userRouter);
+router.use("/items", auth, clothingItemRouter);
 
 router.use((req, res) => {
   res.status(NOT_FOUND_ERROR).send({ message: "Requested resource not found" });
