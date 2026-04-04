c79736766e9dc12da7522521b2dd1d2ce103ab67
webDeveloper42 <rfrias361@gmail.com>
2026-04-04 19:28:25 -0400

    Fix:something done i forgot

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const router = require("./routes/index");
 const errorHandler = require("./middleware/error-handler.js");
 const { errors } = require("celebrate");
+const { requestLogger, errorLogger } = require("./middleware/logger");
 
 const app = express();
 const { port = 3001 } = process.env;
@@ -11,8 +12,10 @@ const { port = 3001 } = process.env;
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 app.use(express.json());
 app.use(cors());
-app.use(router);
 
+app.use(requestLogger);
+app.use(router);
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 if (require.main === module) {
