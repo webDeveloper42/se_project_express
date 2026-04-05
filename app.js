7417f04492a937bba57f83af19d97d957d12cde1
webDeveloper42 <rfrias361@gmail.com>
2026-04-04 23:51:23 -0400

    fix: add crash test

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ app.use(express.json());
 app.use(cors());
 
 app.use(requestLogger);
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
 app.use(router);
 app.use(errorLogger);
 app.use(errors());
